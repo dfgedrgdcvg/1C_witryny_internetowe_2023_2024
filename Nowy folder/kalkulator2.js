4671d3a6d3d8020241b23b77c7493b02e18b4e95
dfgedrgdcvg <144451259+dfgedrgdcvg@users.noreply.github.com>
2024-09-17 14:55:07 +0200

    Add files via upload

---
+++ Nowy folder/kalkulator2.js
@@ -0,0 +1,16 @@
+let liczba1;
+let liczbe2;
+
+liczba1 = prompt("Podaj liczbe 1");
+liczba2 = prompt("Podaj liczbe 2");
+
+let dodawanie = liczba1 + liczba2;
+let odejmowanie = liczba1 - liczba2;
+let mnozenie = liczba1 * liczba2;
+let dzielenie = liczba1 / liczba2;
+
+document.write(dodawanie);
+document.write(odejmowanie);
+document.write(mnozenie);
+document.write(dzielenie);
+
