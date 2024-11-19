6cf48ba675b230b0e9e6db9ad0b493fb66accde8
dfgedrgdcvg <144451259+dfgedrgdcvg@users.noreply.github.com>
2024-11-19 14:31:39 +0100

    Delete 12.11.2024 directory

--- 12.11.2024/zad1.js
+++
@@ -1,7 +0,0 @@
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        document.write(i + " (podzielna przez 3)");
-    } else {
-        document.write(i + " ");
-    }
-}
--- 12.11.2024/zad2.js
+++
@@ -1,7 +0,0 @@
-let suma = 0;
-
-for (let i = 1; i <= 50; i++) {
-    suma += i;
-}
-
-document.write("Suma liczb od 1 do 50 wynosi:", suma);
